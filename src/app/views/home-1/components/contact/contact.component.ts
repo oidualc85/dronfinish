cf7a37f16d5d02c43f181cc8b7ec8761f888ba6a
ClaudioEscobar <claudioescobark@gmail.com>
2025-07-17 15:45:20 -0400

    formulario guarda variables y modificado preloader

--- src/app/views/home-1/components/contact/contact.component.ts
+++ src/app/views/home-1/components/contact/contact.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './contact.component.html',
   styles: ``
 })
+
 export class ContactComponent {
+  public formData = {
+  nombre: '',
+  telefono: '',
+  email: '',
+  mensaje:'',
+  isSubmitted: false
+  };
+
+
+submitForm(): void {
+  console.log('Datos guardados:', this.formData);
 
 }
+
+};
